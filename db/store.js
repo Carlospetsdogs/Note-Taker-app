1bee0b89453763e01993d3907253df9df70a2fa7
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-04-17 13:30:42 -0400

    added more to store.js

--- db/store.js
+++ db/store.js
@@ -4,3 +4,14 @@ const fs = require('fs');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
+class Store {
+    read() {
+        return readFileAsync('db/db.json' , 'utf8');
+    }
+
+    write(note) {
+        return writeFileSync('db/db.json' JSON.stringify(note));
+    }
+
+    
+}
