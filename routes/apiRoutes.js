09d0fff67519394568a4d538d51ecd247b700716
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-04-17 13:11:50 -0400

    added more to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,11 +3,22 @@ const fs = require('fs');
 const path = require('path');
 let db = require('../db/db.json');
 
+
 router.get('/notes' , ( req , res ) => {
     res.json(db)
 });
 
+router.post('/notes' , ( req , res ) => {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.random()
+    }
 
+    db.push(newNote)
+    fs.writeFileSync('./db/db.json' , JSON.stringify(db))
+    res.json(db)
+});
 
 
 
