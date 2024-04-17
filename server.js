7c3f10c418f1fd7042a7f355b2be7b99f5a9313f
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-04-17 12:39:47 -0400

    added to server.js and htmlRoutes.js

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
