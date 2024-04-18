0aa4efaf51cbf0727521b6c5b9564dd389d15b62
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-04-18 10:51:21 -0400

    added note delete function to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,20 +4,28 @@ const path = require('path');
 let db = require('../db/db.json');
 
 // allowing the app to request and respond
-router.get('/notes' , ( req , res ) => {
+router.get('/notes', (req, res) => {
     res.json(db)
 });
 
-router.post('/notes' , ( req , res ) => {
+router.post('/notes', (req, res) => {
     let newNote = {
         title: req.body.title,
         text: req.body.text,
-    
+
     }
 
     db.push(newNote)
-    fs.writeFileSync('./db/db.json' , JSON.stringify(db))
+    fs.writeFileSync('./db/db.json', JSON.stringify(db))
     res.json(db)
+});
+
+
+router.delete('/notes/:id', (req, res) => {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));
 });
 
 
